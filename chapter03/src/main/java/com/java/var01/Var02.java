1a93b52ca37cbe9b8b91299af6a89e80767413e1
WeiMin-Coding <badmanweimin@gmail.com>
2024-08-12 00:02:47 +0800

    变量

---
+++ chapter03/src/main/java/com/java/var01/Var02.java
@@ -0,0 +1,15 @@
+package com.java.var01;
+
+public class Var02 {
+    public static void main(String[] args) {
+        int age = 30;
+        double score = 88.9;
+        char gender = 'M';
+        String name = "Wayne Ming";
+        System.out.println("Info: ");
+        System.out.println("Age: " + age);
+        System.out.println("Score: " + score);
+        System.out.println("Gender: " + gender);
+        System.out.println("Name: " + name);
+    }
+}
