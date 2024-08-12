4d1bf14905bd9283ee0e71cabd2f38680d74f46b
Wayne Ming <badmanweimin@gmail.com>
2024-08-12 11:06:36 +0800

    变量的使用1

--- chapter03/src/main/java/com/java/variable/VariableTest.java
+++ chapter03/src/main/java/com/java/variable/VariableTest.java
@@ -16,9 +16,20 @@ public class VariableTest {
 
         System.out.println("age = " + age);
         System.out.println("gender = " + gender);
+
+        // 在同一个作用域内，不能声明两个同名的变量
+        // char gender = '男';
+
+        // Java中每个变量必须先声明，后使用。
+        // number = 100
+
+        // 超出byte类型范围，编译不通过。
+        // byte b1 = 128;
+        byte b2 = 127;
     }
 
-    public static void main123(String[] args) {
+    public static void main123() {
+        // 变量只有在其作用域内才有效。出了作用域，变量不可以再被调用。
         char gender = 'a';
     }
 }
